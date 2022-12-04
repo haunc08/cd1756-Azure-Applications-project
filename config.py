eaa5f7dc87aa5645f7c957c9a7896282af37292c
haunc08 <18520277@gm.uit.edu.vn>
2022-12-04 21:59:42 +0700

    expose env

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
         'SQL_SERVER') or 'haunc3-udacity-1-server.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'db-1'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'haunc08'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'sql-password'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'okpassword.3'
     SQLALCHEMY_DATABASE_URI = f"mssql+pymssql://{SQL_USER_NAME}:{SQL_PASSWORD}@{SQL_SERVER}/{SQL_DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -24,7 +24,8 @@ class Config(object):
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    CLIENT_SECRET = os.getenv(
+        "CLIENT_SECRET") or '.wP8Q~rKoPsjg--NU1PoTpQ4FHL1sf3wY42XkblY'
     if not CLIENT_SECRET:
         raise ValueError("Need to define CLIENT_SECRET environment variable")
 
